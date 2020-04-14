9c3f47b866d3843ba11ab5d7134f3ba1bc8b2d9d
Rob Orsini <rob.orsini@autodesk.com>
2020-04-14 09:17:16 -0700

    small footer change.

--- src/App.svelte
+++ src/App.svelte
@@ -31,7 +31,7 @@
 <footer class="footer">
     <div class="content has-text-centered">
         <p class="is-size-6">
-            <strong>PyBass</strong> is brought to you by <a href="https://www.tupleshop.com">TupleShop Designs</a>. The source code is licensed under 
+            <strong>PyBass</strong> by <a href="https://www.tupleshop.com">TupleShop</a>. The source code is licensed under
             <a href="https://github.com/rorsini/pybass_svelte/blob/master/LICENSE">GPL-3.0</a>. The website content
             is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
         </p>
